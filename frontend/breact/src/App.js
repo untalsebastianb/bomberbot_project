dc682f5fa71dca18c8d571ef2b9a9da31eecc1e7
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-05 17:26:57 -0500

    Create main app file

--- frontend/breact/src/App.js
+++ frontend/breact/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import TeachersList from '../src/components/TeachersList'
+import useInitialState from './hooks/useInitialState.js'
 
-export const App = () => {
+const API = 'http://localhost:3000/initalState'
+
+
+const App = () => {
+  const teacher = useInitialState(API);
+  console.log(teacher)
   return (
     <div>
-      <h1>let's start</h1>
+      {
+        teacher.teachers?.length > 0 &&
+            <div>
+            {
+                teacher.teachers?.map(item => 
+                <TeachersList key={item.id} {...item} />
+            )}
+            </div>
+      }
     </div>
   )
 }
+export default App
 
