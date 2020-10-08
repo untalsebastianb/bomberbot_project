99dd8e4dd89fbc59b4125a905252192a11366a48
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-08 00:21:25 -0500

    Update main app file and add layout component

---
+++ frontend/breact/src/routes/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import TeachersManager from '../containers/TeachersManager'
+
+
+
+const App = () => (
+  // Aqui viene componente de INFO 
+<Layout>
+<TeachersManager />
+</Layout>
+)
+export default App
