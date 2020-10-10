f6d3436ff036ecbe7a6061ec4b3fe34c69b69825
Oskar Info <racso1607@gmail.com>
2020-10-10 17:52:12 -0500

    Add new component and subcomponents for MenuBar The icons files have been updated, it is still required to update the teachers profile css

--- frontend/breact/src/components/Layout.js
+++ frontend/breact/src/components/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import MenuBar from '../containers/MenuBar'
 // este archivo es el layout que va a seguir la aplicacion
 
 const Layout = ({children}) => (
-// Aqui va el componente que hizo info / compontente/container FIJO ⚛️
-<div className=''>
+<>
+<MenuBar/>
 {/* Aqui van todos los componentes que van a cambiar del layout */}
 {children}
-</div>
+</>
 )
 export default Layout
---
+++ frontend/breact/src/components/StatusLicense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/StatusLicense.scss'
+
+function StatusLicense() {
+    return (
+      <div className='Status-licence'>
+        <p>Status license</p>
+        <div id='icon-status'></div>
+      </div>
+    )
+}
+
+export default StatusLicense;
