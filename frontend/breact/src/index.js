0eda6400c252a509f212a16a4a7f9d227bea4090
somarae8 <somarae8@gmail.com>
2020-10-12 11:41:06 -0500

    remove sendgrid configuration from index.js

--- frontend/breact/src/index.js
+++ frontend/breact/src/index.js
@@ -6,26 +6,5 @@ ReactDOM.render(
     <App/>,
   document.getElementById('app')
 );
-//configuration send grid
-const sendGridAPIKey = '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';
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(sendGridAPIKey)
-
-const msg = {
-  to: 'somarae8@gmail.com', // Change to your recipient
-  from: '1493@holbertonschool.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
 
 
