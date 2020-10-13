ee7076b36f3a6ff184e8a13b50c9e7e43a5cfb71
Oskar Info <racso1607@gmail.com>
2020-10-12 19:11:45 -0500

    remane logo and add logo to right component

--- frontend/breact/src/components/Layout.js
+++ frontend/breact/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MenuBar from '../containers/MenuBar'
+import logo from '../assets/images/bomberbotLogo.png';
 // este archivo es el layout que va a seguir la aplicacion
 
 
@@ -20,6 +21,9 @@ return(
     <MenuBar/>
     <button onClick={handler}>Logout</button>
     <div className='right-container'>
+        <div className='Logo-bomberbot'>
+            <img src={logo} alt='Logo'></img>
+        </div>
         {/* Aqui van todos los componentes que van a cambiar del layout */}
         {children}
     </div>
