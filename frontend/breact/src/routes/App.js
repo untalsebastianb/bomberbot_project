8001086d2f8b61bc7cbba3e2bffde3ac851b0936
JoseR98 <josealonsoingeniero@hotmail.com>
2020-10-13 11:50:29 -0500

    Add Teacher profile component tag

--- frontend/breact/src/routes/App.js
+++ frontend/breact/src/routes/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import TeachersManager from '../containers/TeachersManager'
+import TeacherProfile from '../containers/TeacherProfile'
 import '../assets/styles/App.scss'
 
 
 const App = () => (
 <>
 <Layout>
+<TeacherProfile />
 <TeachersManager />
 </Layout>
 </>
