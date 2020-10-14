d948ff3b9f464da75862a7191a0949ccdf253417
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-14 18:01:12 -0500

    Manipulate some components

--- frontend/breact/src/routes/App.js
+++ frontend/breact/src/routes/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import Layout from '../components/Layout'
 //import HomePage from '../containers/HomePage'
 import TeacherProfile from '../containers/TeacherProfile'
+import TeachersManager from '../containers/TeachersManager'
 //import Comunication from '../containers/Comunications'
 import '../assets/styles/App.scss'
 // import BuyLicenses from '../containers/BuyLicenses'
 
 const App = () => (
 <>
-<Layout>    
+<Layout>
     {/*<HomePage />*/}
-    <TeacherProfile />
-    {/* <TeachersManager /> */}
+    {/* <TeacherProfile /> */}
+    <TeachersManager />
     {/*<Comunication/>*/}
     {/* <BuyLicenses /> */}
 </Layout>
