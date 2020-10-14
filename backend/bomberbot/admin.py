80fbe28cf3cd9d7edb263f8283695fdb85620e38
dairof7 <dairof7@gmail.com>
2020-10-14 16:49:53 -0500

    changes on basedata

--- backend/bomberbot/admin.py
+++ backend/bomberbot/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from bomberbot.models import Teacher, School, Classroom, Lesson, Student
+from bomberbot.models import Teacher, School, Classroom, Lesson
 
 admin.site.register(Teacher)
 admin.site.register(School)
 admin.site.register(Classroom)
 admin.site.register(Lesson)
-admin.site.register(Student)
