7a1dc3e594c46858a1f21e6060144dd62c8358a9
JoseR98 <josealonsoingeniero@hotmail.com>
2020-10-15 13:32:26 -0500

    Add SchoolProfile container to the app

--- frontend/breact/src/routes/App.js
+++ frontend/breact/src/routes/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import Layout from '../components/Layout'
 //import HomePage from '../containers/HomePage'
-import TeacherProfile from '../containers/TeacherProfile'
-import TeachersManager from '../containers/TeachersManager'
+//import TeacherProfile from '../containers/TeacherProfile'
+//import TeachersManager from '../containers/TeachersManager'
 //import Comunication from '../containers/Comunications'
-import '../assets/styles/App.scss'
 // import BuyLicenses from '../containers/BuyLicenses'
+import SchoolProfile from '../containers/SchoolProfile'
+import '../assets/styles/App.scss'
+
 
 const App = () => (
 <>
 <Layout>
     {/*<HomePage />*/}
     {/* <TeacherProfile /> */}
-    <TeachersManager />
+    <SchoolProfile />
+    {/* <TeachersManager /> */}
     {/*<Comunication/>*/}
     {/* <BuyLicenses /> */}
 </Layout>
