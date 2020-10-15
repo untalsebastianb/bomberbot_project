a061aa30766d3f4d0c69e9209c469e53844ea9e5
JoseR98 <josealonsoingeniero@hotmail.com>
2020-10-15 13:28:40 -0500

    Add School profile view with its components. =========================================== Container:
    - SchoolProfile.js
    
    Components:
    
    - DescriptionSchool.js
    - InfoSchool.js

---
+++ frontend/breact/src/components/InfoSchool.js
@@ -0,0 +1,39 @@
+// Component that contains general info of the School such as: country, city, E-mail...
+//import { blue } from '@material-ui/core/colors';
+import React, { useState, useEffect } from 'react'
+import '../assets/styles/components/InfoSchool.scss'
+
+
+const InfoSchool = () => {
+    
+    const [report, setStatus] = useState(false);
+    
+    function status () {
+      if (report) {
+        setStatus(false);
+      }
+      else {
+        setStatus(true);
+      }
+    }
+    useEffect(() => {
+      console.log(report);
+    }, [report]);
+
+    return (
+      <div className="generalInfo">
+        <ul>
+          <li className="Country">Country: </li>
+          <li className="City">City: </li>
+          <li className="Address">Address: </li>
+          <li className="PostCode">Post code: </li>
+          <li className="Email">E-mail: </li>
+          <li className="Telephone">Telephone: </li>
+          <div className="Button" onClick={ status }></div>
+          { report ? <div className="Report">Downnload report</div> : null}
+        </ul>
+      </div>
+    );
+}
+
+export default InfoSchool;
