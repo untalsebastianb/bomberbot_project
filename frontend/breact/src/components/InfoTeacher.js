762b0b7354707729903ddfb21129cd9abc202b9d
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-20 11:52:10 -0500

    Add data to the component

--- frontend/breact/src/components/InfoTeacher.js
+++ frontend/breact/src/components/InfoTeacher.js
@@ -5,18 +5,18 @@ import '../assets/styles/components/InfoTeacher.scss'
 import { Link } from 'react-router-dom'
 
 
-const InfoTeacher = () => {
+const InfoTeacher = ({email, address, phone, age, score}) => {
     
     
     return (
       <div className="generalInfo">
         <ul>
-          <li className="Country">Country: </li>
+          <li className="Country">Age: {age}</li>
           <li className="City">City: </li>
-          <li className="Address">Address: </li>
-          <li className="PostCode">Post code: </li>
-          <li className="Email">E-mail: </li>
-          <li className="Telephone">Telephone: </li>
+          <li className="Address">Address: {address}</li>
+          <li className="PostCode">Score: {score}</li>
+          <li className="Email">E-mail: {email}</li>
+          <li className="Telephone">Telephone: {phone}</li>
           <Link className="Button" to='/teacher_report'/>
 
         </ul>
