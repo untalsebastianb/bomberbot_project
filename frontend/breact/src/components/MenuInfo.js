556eb212390aa476df6337f49715ad5f877a8956
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-21 18:40:31 -0500

    Add photo to MenuBar

--- frontend/breact/src/components/MenuInfo.js
+++ frontend/breact/src/components/MenuInfo.js
@@ -2,12 +2,12 @@ import React from 'react';
 import '../assets/styles/components/MenuInfo.scss'
 import { Link } from 'react-router-dom'
 
-function MenuInfo({children, name}) {
+function MenuInfo({children, name, picture}) {
     return (
       <div className='info'>
         <Link to='/home'>
         <div id='school-logo'>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/The_Woodlands_College_Park_Front_Image.jpg/220px-The_Woodlands_College_Park_Front_Image.jpg" alt="imagen"/>
+          <img src={picture} alt="imagen"/>
           <p>Home</p>
         </div>
         </Link>
