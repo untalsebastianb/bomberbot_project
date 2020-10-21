714cc6bd1e5203c7d4c59d0f5e00d3b82d5637da
JoseR98 <josealonsoingeniero@hotmail.com>
2020-10-21 14:44:59 -0500

    Add Teacher report functionality

--- frontend/breact/src/components/TeacherReport.js
+++ frontend/breact/src/components/TeacherReport.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import useInitialState from '../hooks/teacherReport.js';
 import Typography from '@material-ui/core/Typography';
+import '../assets/styles/components/TeacherReport.scss'
 
 const TeacherReport = (props) => {
     const teacherId = props.id;
@@ -13,6 +14,35 @@ const TeacherReport = (props) => {
         <Typography variant='h4' className="TeacherName">
             Teacher Report
         </Typography>
+        <hr className="titleSpace"/>
+        <table className="globalTable">
+        <tr className="GlobalHeaders">
+            <th>Name</th>
+            <th>Total Students</th>
+            <th>Score</th>
+        </tr>
+        <tr className="GlobalValues">
+            <td>{ report.name }</td>
+            <td>{ report.total_students }</td>
+            <td>{ report.score }</td>
+        </tr>
+        </table>
+        <br className="table1Space"/>
+        <table className="classesTable">
+        <tr className="classesHeaders">
+            <th>Class Name</th>
+            <th>Number Lessons</th>
+            <th>Total Students</th>
+            <th>Average Score</th>
+        </tr>
+        { report.classrooms?.map(item => 
+        <tr>
+            <td>{item.name}</td>
+            <td>{item.lessons}</td>
+            <td>{item.total_students}</td>
+            <td>{item.average_score}</td>
+        </tr>) }
+        </table>
         </div>
     )
 }
