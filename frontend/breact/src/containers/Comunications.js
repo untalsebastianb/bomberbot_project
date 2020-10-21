93d7defbab793fd1c0da3565dbbec7281ebe37d5
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-21 15:04:14 -0500

    Fix styles

--- frontend/breact/src/containers/Comunications.js
+++ frontend/breact/src/containers/Comunications.js
@@ -1,4 +1,3 @@
-import { Typography } from '@material-ui/core';
 import React from 'react'
 import FormComunication from '../components/FormComunication'
 import Typography from '@material-ui/core/Typography';
